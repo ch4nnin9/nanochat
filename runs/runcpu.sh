c78fe4e25f1477ce4cbfc0ff6e29c07519f15112
Channing <channingwang2211@gmail.com>
2026-07-24 17:31:56 +1200

    adjust batch size for local training

--- runs/runcpu.sh
+++ runs/runcpu.sh
@@ -48,7 +48,7 @@ python -m scripts.base_eval --device-batch-size=1 --split-tokens=16384 --max-per
 curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
 python -m scripts.chat_sft \
     --max-seq-len=512 \
-    --device-batch-size=32 \
+    --device-batch-size=16 \
     --total-batch-size=16384 \
     --eval-every=200 \
     --eval-tokens=524288 \
